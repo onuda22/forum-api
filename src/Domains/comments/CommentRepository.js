399e7c645aaa552dc5e683628b78bc3cc3e6913b
onuda22 <okinabada@gmail.com>
2025-06-01 20:01:24 +0700

    feat(domain): create comment repository interface

---
+++ src/Domains/comments/CommentRepository.js
@@ -0,0 +1,11 @@
+class CommentRepository {
+  async addComment(comment) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteCommentById(id) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = CommentRepository;
