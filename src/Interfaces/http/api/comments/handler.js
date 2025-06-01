ef79f7bddbb3a076f206312bd459b8129647701f
onuda22 <okinabada@gmail.com>
2025-06-01 23:57:52 +0700

    feat(interface): implements route comments

---
+++ src/Interfaces/http/api/comments/handler.js
@@ -0,0 +1,40 @@
+const AddCommentUseCase = require('../../../../Applications/use_case/AddCommentUseCase');
+
+class CommentsHandler {
+  constructor(container) {
+    this._container = container;
+
+    this.postCommentHandler = this.postCommentHandler.bind(this);
+  }
+
+  async postCommentHandler(request, h) {
+    const addCommentUseCase = this._container.getInstance(
+      AddCommentUseCase.name
+    );
+
+    const { id: owner } = request.auth.credentials;
+    const { content } = request.payload;
+    const { threadId } = request.params;
+
+    const payload = {
+      content,
+      owner,
+      threadId,
+    };
+
+    const addedComment = await addCommentUseCase.execute(payload);
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        addedComment,
+      },
+    });
+
+    response.code(201);
+
+    return response;
+  }
+}
+
+module.exports = CommentsHandler;
